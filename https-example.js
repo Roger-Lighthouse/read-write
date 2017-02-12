94abe976da820291f3f42d714afa473d9ee2d1fa
Roger-Lighthouse <roger@whiteshark.ca>
2017-02-12 20:31:20 +0000

    Now connected response and showed html

--- https-example.js
+++ https-example.js
@@ -1,16 +1,23 @@
 var https = require('https');
 
-console.log('I did it!');
+//console.log('I did it!');
 
 var options = {
-  host: 'office.whiteshark.ca',
-  path: '/'
+  //host: 'office.whiteshark.ca',
+  //path: '/'
+  host: 'stream-large-file.herokuapp.com',
+  path: '/give-me-stuff-now'
 };
 
 // called by https when the request is made.
 var callback = function(response) {
   console.log('In response handler callback!');
-  console.log('Response: ', response);
+  //console.log('Response: ', response);
+
+  response.on('data', function(chunk) {
+  console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
+ // console.log(chunk.toString());
+  });
 }
 
 console.log("I'm about to make the request!");
@@ -18,3 +25,15 @@ console.log("I'm about to make the request!");
 https.request(options, callback).end();
 
 console.log("I've made the request!");
+
+
+
+
+/*
+QUESTIONS:
+1) Order of execution
+2) Where did response come from in callback function
+3) chunk??
+
+
+*/
